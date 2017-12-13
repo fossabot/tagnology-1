966ae6c9d97472757b78886e0c5ef41f42570676
PK Chuah <pkchuah@seekasia.com>
2017-12-13 20:01:41 +0800

    sync

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -20,20 +20,27 @@ class JobController extends Controller
         $arr_key = ($request->session()->get('key')) ? $request->session()->get('key') : [];
         $keywords = ($request->key) ? $request->key : '';
 
-            //call cloudsearch
+        //call cloudsearch
         $client = new Client(); //GuzzleHttp\Client
         // get result
-        if($keywords) {
+        if ($keywords) {
 
-            array_push($arr_key, $keywords);
+            if (!in_array($keywords, $arr_key)) {
+                array_push($arr_key, $keywords);
+            }
+            
             $request->session()->put('key', $arr_key);
-            $result = $client->request('GET', 'http://search-tagnology-jobs-dxyvozvy5yf53gbqqwvbwrpc44.ap-southeast-1.cloudsearch.amazonaws.com/2013-01-01/search?', [
-                'query' => ['q' => implode(",", $arr_key)]
-            ]);
+            $result = $client->request('GET',
+                'http://search-tagnology-jobs-dxyvozvy5yf53gbqqwvbwrpc44.ap-southeast-1.cloudsearch.amazonaws.com/2013-01-01/search?',
+                [
+                    'query' => ['q' => implode(",", $arr_key)]
+                ]);
         } else {
-            $result = $client->request('GET', 'http://search-tagnology-jobs-dxyvozvy5yf53gbqqwvbwrpc44.ap-southeast-1.cloudsearch.amazonaws.com/2013-01-01/search?', [
-                'query' => ['q' => 'matchall' ,'q.parser'=>'structured']
-            ]);
+            $result = $client->request('GET',
+                'http://search-tagnology-jobs-dxyvozvy5yf53gbqqwvbwrpc44.ap-southeast-1.cloudsearch.amazonaws.com/2013-01-01/search?',
+                [
+                    'query' => ['q' => 'matchall', 'q.parser' => 'structured']
+                ]);
         }
 
         // pass to data
@@ -42,7 +49,7 @@ class JobController extends Controller
         $suggests = ['Gym', 'Dental'];
 
 
-        return view('welcome', compact('data', 'keywords' , 'arr_key'));
+        return view('welcome', compact('data', 'keywords', 'arr_key'));
     }
 
     /**
@@ -75,11 +82,10 @@ class JobController extends Controller
     }
 
 
-
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -90,7 +96,7 @@ class JobController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -101,7 +107,7 @@ class JobController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -112,8 +118,8 @@ class JobController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -124,7 +130,7 @@ class JobController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
